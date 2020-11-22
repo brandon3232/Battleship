5a406f0d2e12cb665fbbac98424180cf59c936f3
brandon3232 <brandongg.3232@gmail.com>
2020-11-21 20:58:26 -0600

    se construyeron las clases barco y seccion

--- src/Barco.java
+++ src/Barco.java
@@ -1,3 +1,50 @@
 public class Barco {
+    private int coordenadaX;
+    private int coordenadaY;
+    private boolean vivo=true;
+    Seccion[] secciones= new Seccion[3];
+
+    public int getX() {
+        return coordenadaX;
+    }
+    public int getY() {
+        return coordenadaY;
+    }
+    public boolean getVivo(){
+        return vivo;
+    }
+    public void setCoordenadaX(int coordenadaX) {
+        this.coordenadaX = coordenadaX;
+    }
+    public void setCoordenadaY(int coordenadaY) {
+        this.coordenadaY = coordenadaY;
+    }
+    public void setVivo(boolean vivo) {
+        this.vivo = vivo;
+    }
+    //metodo que busca la seccion en las coordenadas que se le pasen y cambia el estado de 
+    // la variable funcional de esa seccion a false indicando que a sido undida
+    public void modificaFuncionalSeccion(int x, int y){
+        for (int i = 0; i < 3; i++) {
+            if (secciones[i].getCoordenadaX()==x && secciones[i].getCoordenadaY()==y)  {
+                secciones[i].setFuncional(false);
+            }
+        }
+    }
+    //metodo que verifica todas las secciones de cada barco, si las 3 estan
+    //undidas cambiarà el estado de la variable vivo del barco a false
+    //indicando que este barco ya a sido undido
+    public void actualizarVivo(){
+        int undidosTotales=0;
+        for (int i = 0; i < secciones.length; i++) {
+            if (!(secciones[i].getFuncional())) {
+                undidosTotales+=1;
+            }
+        }
+        if (undidosTotales==3) {
+            vivo=false;
+        }
+    }
     
+
 }
--- src/Seccion.java
+++ src/Seccion.java
@@ -1,3 +1,24 @@
 public class Seccion {
-    
+    private int coordenadaY;
+    private int coordenadaX;
+    private boolean funcional=true;    
+
+    public int getCoordenadaX() {
+        return coordenadaX;
+    }
+    public int getCoordenadaY() {
+        return coordenadaY;
+    }
+    public boolean getFuncional(){
+        return funcional;
+    }
+    public void setCoordenadaX(int coordenadaX) {
+        this.coordenadaX = coordenadaX;
+    }
+    public void setCoordenadaY(int coordenadaY) {
+        this.coordenadaY = coordenadaY;
+    }
+    public void setFuncional(boolean funcional) {
+        this.funcional = funcional;
+    }    
 }
