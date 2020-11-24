39dcc3b28af5110ca1e806aa0d4c956f53a032f3
brandon.3232 <brandongg.3232@gmail.com>
2020-11-23 23:55:17 -0600

    se empezó a crear el main de la clase battleship y avances de la clase Usuario

--- src/Battleship.java
+++ src/Battleship.java
@@ -1,3 +1,67 @@
+import java.util.Scanner;
+
 public class Battleship {
-    
+    public static void main(String args []){
+        int desicion;
+        int tiroX, tiroY, numTiros=10;
+        Scanner scanner = new Scanner (System.in);
+        
+          System.out.println("-------------BATTLESHIP----------");
+          System.out.println("---------------MENU--------------");
+          System.out.println("¿Que desea hacer?");
+          System.out.println("1. Jugar");
+          System.out.println("2. Ver puntajes");
+          System.out.println("3. Salir");
+          desicion = scanner.nextInt(); 
+          
+        
+        switch (desicion){
+            case 1:
+            int n = (int) (Math.random() * (5 - 1)) + 1;
+            if (n == 1){
+                //Poner aqui las instrucciones para abrir el primer archivo
+                if (n == 2){
+                    //Poner aqui las instrucciones para abrir el segundo archivo
+                    if (n == 3){
+                        //Poner aqui las instrucciones para abrir el tercer archivo
+                        if (n == 4){
+                            //Poner aqui las instrucciones para abrir el cuarto archivo
+                        }
+                    }
+                }
+            }
+            //Mandar a traer el metodo usuario para registrarlo
+            //Mandar a traer el metodo para imprimir el tablero
+            do{
+            System.out.println("Seleccione la linea en la que desea tirar:");
+            tiroX = scanner.nextInt();
+            System.out.println("Seleccione la columna en la que desea tirar:");
+            tiroY = scanner.nextInt();
+            //Mandar a traer el metodo de actualiza tablero, tambien en ese metodo se dice si acerto o no
+            if (//Si acerto el tiro)
+                {
+                    //Mandar a traer el metodo que checa las partes del barco
+                }else
+                {
+                    numTiros = numTiros - 1;
+                }
+            //Actualizar tablero    
+            }while (numTiros!=0 || numBarcosHundidos == 10);
+            if (numBarcosHundidos == 10)
+            {
+                System.out.println ("FELICIDADES...!!!!! , Haz hundido todos los barcos");
+                //Manda a traer el metodo para guardar los puntajes
+            }else{
+                System.out.println ("GAME OVER, haz agotado todos tus tiros ): ");
+                //Manda a traer el metodo para guardar los puntajes
+            }
+            
+            break;
+            case 2:
+            //Mandar a traer el metodo que muestra a los usuarios ingresados con sus puntuajes
+            case 3: 
+             System.exit(0);
+        }
+        
+    }
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,28 +1,66 @@
-import java.lang.*;
 import java.util.*;
 import java.util.Scanner.*;
+import java.io.*;
+import java 
 
 
-public class Usuario { 
+public class Usuario implements Serializable
+{ 
+    String nombre;
+    int edad;
+    String ganador;
+    int barcosHundidos;
 
-    public static void main (String args [])
-    {
-        Scanner lee=new Scanner (System.in);
-        String nombre;
-        int edad;
-        System.out.println("Hola y Bienvenido seas a Battleship");
-        System.out.println("¿Cual es su nombre?");
-        nombre=lee.nextLine();
-        System.out.println("¿Cual es su edad?");
-        edad=lee.nextInt();
-
-        if (edad>=18)
+        public boolean pedirDatos()
         {
-            System.out.println("Que disfrutes el juego");
-        }
-            else 
+            Scanner lee=new Scanner (System.in);
+            System.out.println("Hola y Bienvenido seas a Battleship");
+            System.out.println("¿Cual es su nombre?");
+            nombre=lee.nextLine();
+            try{
+            System.out.println("¿Cual es su edad?");
+            edad=lee.nextInt();
+            } catch(InputMismatchException e) {System.out.println("ERROR: no es un numero ENTERO");}
+
+            if (edad>=18)
             {
-                System.out.println ("LO SENTIMOS, el juego solo es para personas mayores de 18 años");
+                System.out.println("Que disfrutes el juego");
+                return true;
             }
-    }
+                else 
+                {
+                    System.out.println ("LO SENTIMOS, el juego solo es para personas mayores de 18 años.");
+                    return false;
+                }
+        }
+        
+        //public void guardaUsuario() throws IOException
+        //{
+            //String nombre;
+            //Scanner lee= new Scanner(System.in);
+            //System.out.println("Dame el nombre del archivo binario: ");
+            //nombre = lee.nextLine();
+            
+            //DataOutputStream f = new DataOutputStream (new FileOutputStream(nombre));
+            
+            //f.close();
+        //}
+         
+        public void LeerArch () 
+        {
+        //private String nombre="temporal.dat";
+        try{
+            ObjectInputStream f =new ObjectInputStream(new FileInputStream(this.ruta_a));
+        }
+
+
+        }
+
+
+        
+        public static void main (String [] args)
+        {
+               
+        }
+
 }
